f3652dc4cb59342175fa7aef09030633af74afe0
Kennethber <kennethber7@gmail.com>
2024-10-11 10:51:33 -0600

    cambios

---
+++ src/Clases/Transaccion.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+/**
+ *
+ * @author HP
+ */
+public class Transaccion {
+    
+    
+  
+    private String descripcion;
+    private double monto;
+
+    public Transaccion(String descripcion, double monto) {
+        this.descripcion = descripcion;
+        this.monto = monto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+    
+}
